3922286286ea8c288f992987582db733dc89844a
Douglas Morais <douglas.morais@4all.com>
2020-05-12 16:32:56 -0300

    Fix switch log type

--- json_log/__init__.py
+++ json_log/__init__.py
@@ -4,12 +4,11 @@ from distutils.util import strtobool
 from dotenv import load_dotenv
 load_dotenv()
 
-local_log_env = strtobool(str(environ.get('LOCAL_LOG', 'True')))
-force_local_env = strtobool(str(environ.get('FORCE_LOCAL_LOG', 'False')))
+use_fluentd = strtobool(str(environ.get('LOG_FLUENTD', 'False')))
 
-if local_log_env or force_local_env:
-    from .file_log import applog
-else:
+if use_fluentd:
     from .fluentd_log import applog
+else:
+    from .file_log import applog
 
 applog = applog
